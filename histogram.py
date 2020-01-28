1eca53d0c1591cbe5313db60af84b3d6be6bc597
Constantino <muheec@gmail.com>
2020-01-28 11:33:59 -0800

    histogram.py

--- histogram.py
+++ histogram.py
@@ -1,4 +1,5 @@
 import re
+import sys
 
 corpus = "paragraph.txt"
 filehandle = open(corpus, "r")
@@ -9,16 +10,41 @@ def clean(corpus):
         scrubbed_words = re.sub(r'[^a-zA-Z\s]', '', corpus).lower().split()
         return scrubbed_words
 
-histogram = {}
+histo = {}
 
-for word in clean(corpus):           
-    if word in histogram: 
-        histogram[word] += 1
-    else: 
-        histogram[word] = 1
+def histogram():
+    for word in clean(corpus):           
+        if word in histo: 
+            histo[word] += 1
+        else: 
+            histo[word] = 1
+
+    return histo
+
+def unique():
+    used_words = []
+    unique_words = 1
+
+    for word in clean(corpus):
+        if word not in used_words:
+            unique_words = unique_words + 1
+            used_words.append(word)
+
+    return unique_words
+
+
+def frequency(search):
+    if search in histo:
+        return 'The word ' + str(search) + ' occurs ' + str(histo[search]) + ' times!'
+    else:
+        return 'Word does not occur'
 
 if __name__ == '__main__':
     # print(clean(corpus))
     # print(histogram)
-    for word in histogram:
-        print(word, histogram[word])
+    for word in histogram():
+        print(word, histo[word])
+
+    print(unique())
+    print(frequency(sys.argv[1]))
+    
