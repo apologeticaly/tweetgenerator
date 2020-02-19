1bce31347cc3bf0b4a182bcc8418721262c12a50
Constantino <muheec@gmail.com>
2020-02-19 14:39:33 -0800

    procfile

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, Response, request, redirect, url_for
 from simple_sample import run
+
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def hello_world():
+    return ' '.join(run())
+
 if __name__ == "__main__":
-    @app.route('/')
-    def hello_world():
-        return ' '.join(run())
+    app.hello_world(debug=True)
