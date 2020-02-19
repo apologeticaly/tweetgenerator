86bfb902c87a8ddbc7842cb76af959c70970e5a2
Constantino <muheec@gmail.com>
2020-02-19 14:37:24 -0800

    procfile

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from simple_sample import run
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return ' '.join(run())
+if __name__ == "__main__":
+    @app.route('/')
+    def hello_world():
+        return ' '.join(run())
